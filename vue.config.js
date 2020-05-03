882ebb768907c3dc1d66860375af137588eaec85
Henrique Gomide <henriquepgomide@gmail.com>
2020-05-03 11:39:21 -0300

    improve manifest versioning

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  pwa:{
+    themeColor: '#209cee',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+    },
+  }
 };
